39302f4caa226ec787c756d6dc229555284a0e44
Sednek <sednek2@yandex.ru>
2023-10-22 16:14:21 +0300

    Faculty and Student add

---
+++ src/main/java/ru/hogwarts/school/model/Faculty.java
@@ -0,0 +1,17 @@
+package ru.hogwarts.school.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Faculty {
+
+    private Long id;
+    private String name;
+    private String color;
+
+}
